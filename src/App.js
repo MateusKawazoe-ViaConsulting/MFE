79aa95b72e513178769c173ce12ba6643a8b0397
Mateus <mateus.kawazoe@viaconsulting.com.br>
2022-05-13 19:38:08 -0300

    feat: create react app and initial configs

---
+++ src/App.js
@@ -0,0 +1,12 @@
+import DinamicForm from './DinamicForm';
+import './app.css';
+
+function App() {
+  return (
+    <div className="App mfe-container justify-center flex pt-14 from-indigo-500 via-purple-500 to-pink-500 bg-gradient-to-r h-screen">
+      <DinamicForm />
+    </div>
+  );
+}
+
+export default App;
